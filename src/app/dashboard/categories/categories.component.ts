53954492791aa8a00d870bd0d5b18673d25ca5c9
Kizito <kizito.azubuike1@gmail.com>
2024-08-06 22:32:48 +0100

    complete categories UI

--- src/app/dashboard/categories/categories.component.ts
+++ src/app/dashboard/categories/categories.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent {
+  showModal: boolean;
+  showAddModal: boolean;
 
+  constructor() {
+    this.showModal = false;
+    this.showAddModal = false;
+  }
+
+  toggleModal(value: boolean) {
+    this.showModal = value;
+  }
+
+  toggleAddModal(value: boolean) {
+    this.showAddModal = value;
+  }
 }
